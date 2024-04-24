c3cd3c825f0e2cfd119a196b63cb2e5673d1b56d
Noyantis-JamieH <167077347+Noyantis-JamieH@users.noreply.github.com>
2024-04-24 13:51:13 +0100

    Added type hints to class methods
    also removed unnecessary imports

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import os, random, json
-from spotifyapi import SpotifyAPI, SpotifyArtist
+from spotifyapi import SpotifyAPI
 
 CLIENT_ID = os.environ['CLIENT_ID']
 CLIENT_SECRET = os.environ['CLIENT_SECRET']
 
 spotify = SpotifyAPI(CLIENT_ID,CLIENT_SECRET)
 
-
+print(json.dumps(spotify.search_artist("James Marriott"), indent=4))
--- spotifyapi.py
+++ spotifyapi.py
@@ -1,7 +1,8 @@
 from urllib.parse import urlencode
-import base64,requests, datetime, random, json, string
+import base64,requests, datetime
+
 class SpotifyAPI:
-    def __init__(self, client_ID,client_secret):
+    def __init__(self, client_ID: str, client_secret: str) -> None:
         self.CLIENT_ID = client_ID
         self.CLIENT_SECRET = client_secret
         
@@ -10,7 +11,7 @@ class SpotifyAPI:
 
         self.perform_auth()
         
-    def perform_auth(self):
+    def perform_auth(self) -> bool:
         token_url = "https://accounts.spotify.com/api/token"
         token_data = {"grant_type" : "client_credentials"}
         token_headers = {"Authorization": f"Basic {self.client_creds_b64.decode()}"}
@@ -30,46 +31,48 @@ class SpotifyAPI:
             return True
         else: raise Exception("Authentication failed")
 
-    def get_access_token(self):
+    def _get_access_token(self) -> str:
         token = self.access_token
         expires = self.access_token_expires
         now = datetime.datetime.now()
         if expires < now:
             self.perform_auth()
-            return self.get_access_token()
+            return self._get_access_token()
         return token
     
-    def get_resource_header(self):
-        access_token = self.get_access_token()
+    def _get_resource_header(self) -> dict:
+        access_token = self._get_access_token()
         return {"Authorization" : f"Bearer {access_token}"}
     
-    def get_resource(self,_id,resource_type):
+    def _get_resource(self,_id: str, resource_type: str) -> dict:
         endpoint = f"https://api.spotify.com/v1/{resource_type}/{_id}"
-        headers = self.get_resource_header()
+        headers = self._get_resource_header()
         r = requests.get(endpoint, headers=headers)
         if r.status_code not in range(200,299):
             return {}
         return r.json()
     
-    def search(self,query,search_type):
-        headers = self.get_resource_header()
+    def search(self,query: str, search_type: str) -> dict:
+        headers = self._get_resource_header()
         endpoint = "https://api.spotify.com/v1/search"
         data = urlencode({"q": f"{query}","type": f"{search_type.lower()}"})
         url = f'{endpoint}?{data}'
         r = requests.get(url, headers=headers)
-        if r.status_code in range(200,299): return r.json()
+        if r.status_code in range(200,299):
+            return r.json()
 
-    def get_album(self, _id):
-        return self.get_resource(_id,'albums')
+    def get_album(self, _id: str) -> dict:
+        return self._get_resource(_id,'albums')
 
-    def get_artist(self, _id):
-        return self.get_resource(_id,'artists')
+    def get_artist(self, _id: str) -> dict:
+        return self._get_resource(_id,'artists')
 
-    def search_artist(self, query,result=1):
+    #TODO: Remake these
+    def search_artist(self, query:str ,result: int=1) -> dict:
         try: return self.get_artist(self.search(query,'artist')['artists']['items'][result-1]['id'])
         except TypeError: return False
 
-    def search_album(self, query,result=1):
+    def search_album(self, query: str, result: int=1) -> dict:
         try: return self.get_album(self.search(query,'album')['albums']['items'][result-1]['id'])
         except TypeError: return False
 
